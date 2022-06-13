c059a3163ea65a05a1d93b1000af8de91e223670
Igorttdp <igorttdp.dev@email.com>
2022-06-13 09:56:40 -0300

    FEAT close btn on results div

---
+++ README.md
@@ -0,0 +1,7 @@
+# Calculadora-IMC
+
+<h2>Preview</h2>
+<div><img src="assets/img/preview.gif" alt="Preview Project"/></div>
+
+<h3>Sobre</h3>
+<p>Calculadora do IMC (BMI) feita com javascript manipulando elementos no HTML</p>
---
+++ assets/js/main.js
@@ -0,0 +1,71 @@
+
+
+function funcaoGeral() {
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultadoIMC');
+    const textoResult = document.querySelector('#p-result')
+
+    function recebeEventoForm(evento) {
+        evento.preventDefault();
+        const peso = parseFloat(document.body.querySelector("#peso").value); //.value POIS TEM QUE PEGAR O VALOR, NÃO A SELEÇÃO
+        const altura = parseFloat(document.body.querySelector("#altura").value); //.value POIS TEM QUE PEGAR O VALOR, NÃO A SELEÇÃO
+
+
+        if (Number.isNaN(peso) === true || Number.isNaN(altura) === true) {
+            resultado.style.backgroundColor = '#ad0000';
+            resultado.style.color = '#fff'
+            textoResult.innerHTML = `Valor(es) inválido(s)`
+            abrir();
+            return;
+        } else {
+            abrir();
+            calculoIMC();
+        }
+
+        function calculoIMC() {
+            const imc = peso / (altura * altura);
+            if (imc < 18.5) {
+                resultado.style.backgroundColor = '#ff4c4c';
+                resultado.style.color = '#fff'
+                textoResult.innerHTML = `Seu IMC é ${imc.toFixed(2)} (Abaixo do peso)`
+            } else if (imc >= 18.5 && imc <= 24.9) {
+                resultado.style.backgroundColor = '#01FEC3';
+                resultado.style.color = '#000'
+                textoResult.innerHTML = `Seu IMC é ${imc.toFixed(2)} (Peso normal)`
+            } else if (imc >= 25 && imc <= 29.9) {
+                resultado.style.backgroundColor = '#fcfc9c';
+                resultado.style.color = '#000'
+                textoResult.innerHTML = `Seu IMC é ${imc.toFixed(2)} (Sobrepeso)`
+            } else if (imc >= 30 && imc <= 34.9) {
+                resultado.style.backgroundColor = '#ff4c4c';
+                resultado.style.color = '#fff'
+                textoResult.innerHTML = `Seu IMC é ${imc.toFixed(2)} (Obesidade grau 1)`
+            } else if (imc >= 35 && imc <= 39.9) {
+                resultado.style.backgroundColor = '#fc0000';
+                resultado.style.color = '#fff'
+                textoResult.innerHTML = `Seu IMC é ${imc.toFixed(2)} (Obesidade grau 2)`
+            } else {
+                resultado.style.backgroundColor = '#ad0000';
+                resultado.style.color = '#fff'
+                textoResult.innerHTML = `Seu IMC é ${imc.toFixed(2)} (Obesidade grau 3)`
+            }
+        }
+    }
+
+    function abrir() {
+        const resultado = document.querySelector('.resultadoIMC');
+        const fecharResultado = document.querySelector('.close-result')
+        fecharResultado.style.display = 'block'
+        resultado.style.display = 'flex'
+    }
+
+    form.addEventListener('submit', recebeEventoForm); //EventListener pega qualquer evento do ALVO. Nesse caso, o FORM
+}
+
+funcaoGeral() // CHAMAR A %#¨#@ DA FUNÇÃO PARA DAR PREVENT DEFAULT SENÃO NÃO FUNCIONA SEU CORNO
+
+function fechar() {
+    const resultado = document.querySelector('.resultadoIMC');
+    resultado.style.display = 'none'
+}
+
